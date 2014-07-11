5b3765b7126bbb303d3ae598c6ad0a806aa2f0be
tommy nguyen <tommynguyen987@gmail.com>
2014-07-11 16:59:41 +0700

    MVC4 Demo

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        public string Browse(string genre)
+        {
+            //return "Hello from Store.Browse()";
+
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message;
+        }
+
+        public string Details(int id)
+        {
+            //return "Hello from Store.Details()";
+
+            string message = HttpUtility.HtmlEncode("Store.Details, ID = " + id);
+            return message;
+        }
+
+    }
+}
---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public virtual int AlbumId { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual int ArtistId { get; set; }
+        public virtual string Title { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
